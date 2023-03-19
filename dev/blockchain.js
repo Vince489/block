1c0d728ee4b767c46b74135eea5c7d988d347002
Vince489 <vincentcornelius489@gmail.com>
2023-03-18 22:03:17 -0400

    check

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,5 +1,6 @@
 const sha256 = require('sha256');
 const currentNodeUrl = process.argv[3];
+const { v1: uuidv1 } = require('uuid');
 
 class Blockchain {
   constructor() {
@@ -32,15 +33,24 @@ class Blockchain {
     return this.chain[this.chain.length - 1];
   }
 
-  createNewTransaction(amount, sender, recipient) {
-    const newTransaction = {
-      amount: amount,
-      sender: sender,
-      recipient: recipient,
-    };
+  createNewTransaction(amount, senderWallet, recipient) {
+    const newTransaction = new Transaction(senderWallet.publicKey, recipient, amount);
+    newTransaction.timestamp = Date.now();
+    newTransaction.transactionId = uuidv1();
+    newTransaction.sender = senderWallet.publicKey;
+    newTransaction.signature = senderWallet.signTransaction(newTransaction);
+  
+    const isValid = this.verifyTransaction(newTransaction);
+    if (!isValid) {
+      throw new Error('Invalid transaction!');
+    }
+  
+    return newTransaction;
+  }
+  
 
-    this.pendingTransactions.push(newTransaction);
-
+  addTransactionToPendingTransactions(transactionObj) {
+    this.pendingTransactions.push(transactionObj);
     return this.getLastBlock()['index'] + 1;
   }
 
@@ -59,6 +69,94 @@ class Blockchain {
       hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
     }
     return nonce;
+  }
+
+  chainIsValid (blockchain) {
+    let validChain = true;
+
+    for (var i = 1; i < blockchain.length; i++) {
+      const currentBlock = blockchain[i];
+      const prevBlock = blockchain[i - 1];
+      const blockHash = this.hashBlock(prevBlock['hash'], { transactions: currentBlock['transactions'], index: currentBlock['index'] }, currentBlock['nonce']);
+
+      if (blockHash.substring(0, 4) !== '0000') validChain = false;
+      if (currentBlock['previousBlockHash'] !== prevBlock['hash']) validChain = false;
+    };
+
+    const genesisBlock = blockchain[0];
+    const correctNonce = genesisBlock['nonce'] === 100;
+    const correctPreviousBlockHash = genesisBlock['previousBlockHash'] === '0';
+    const correctHash = genesisBlock['hash'] === '0';
+    const correctTransactions = genesisBlock['transactions'].length === 0;
+
+    if (!correctNonce || !correctPreviousBlockHash || !correctHash || !correctTransactions) validChain = false;
+
+    return validChain;
+  }
+
+  getBlock(blockHash) {
+    let correctBlock = null;
+    this.chain.forEach(block => {
+      if (block.hash === blockHash) correctBlock = block;
+    });
+    return correctBlock;
+  }
+
+  getTransaction(transactionId) {
+    let correctTransaction = null;
+    let correctBlock = null;
+
+    this.chain.forEach(block => {
+      block.transactions.forEach(transaction => {
+        if (transaction.transactionId === transactionId) {
+          correctTransaction = transaction;
+          correctBlock = block;
+        };
+      });
+    });
+
+    return {
+      transaction: correctTransaction,
+      block: correctBlock
+    };
+  }
+
+  getAddressData(address) {
+    const addressTransactions = [];
+    this.chain.forEach(block => {
+      block.transactions.forEach(transaction => {
+        if (transaction.sender === address || transaction.recipient === address) {
+          addressTransactions.push(transaction);
+        };
+      });
+    });
+
+    let balance = 0;
+    addressTransactions.forEach(transaction => {
+      if (transaction.recipient === address) balance += transaction.amount;
+      else if (transaction.sender === address) balance -= transaction.amount;
+    });
+
+    return {
+      addressTransactions: addressTransactions,
+      addressBalance: balance
+    };
+  }
+}
+
+class Transaction {
+  constructor(sender, recipient, amount) {
+    this.sender = sender;
+    this.recipient = recipient;
+    this.amount = amount;
+    this.timestamp = Date.now();
+    this.transactionId = uuidv1();
+  }
+
+  calculateHash() {
+    const dataAsString = this.sender + this.recipient + this.amount + this.timestamp;
+    const hash = sha256(dataAsString);
+    return hash;
   }
 }
 
